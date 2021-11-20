3e7e452b4585b4a1913898525c7fcf7045c02b4c
Hannah Bish <hvbish@uw.edu>
2021-11-19 16:33:49 -0800

    option to use peak velocity

--- fitmodel.py
+++ fitmodel.py
@@ -165,7 +165,7 @@ if __name__ == '__main__':
 
     run_in_parallel = True # If False, code will use a single thread (takes much longer)
     lat_lim = 60 # Only fit sightlines below this Galactic latitude
-    velocity_method = 'maxcol' # How to calculate sightline velocities. Possible values: 'Nweighted', 'maxcol'
+    velocity_method = 'maxcol' # How to calculate sightline velocities. Possible values: 'weighted', 'maxcol'
 
     HVC_flag = '3' # Choose which HVC flag we want
 
@@ -221,17 +221,19 @@ if __name__ == '__main__':
         print ("Running " + densmod.__name__ + " model...")
         
         # Reading in sightlines
-        if velocity_method == 'Nweighted':
+        if velocity_method == 'weighted':
             ds = pd.read_table("data/sightlines_flag_" + HVC_flag + ".txt", sep=' ', skipinitialspace=True)
         elif velocity_method == 'maxcol':
             ds = pd.read_table("data/sightlines_maxN_flag_" + HVC_flag + ".txt", sep=' ', skipinitialspace=True)
+        elif velocity_method == 'peakvel':
+            ds = pd.read_table("data/sightlines_peakvel_flag_" + HVC_flag + ".txt", sep=' ', skipinitialspace=True)
         else:
             raise NameError('Please choose a valid velocity calculation method!')
 
         # We select only the ion we have chosen to fit
         di = ds[ds['ion']==ion]
         # We select only latitudes below 60 deg for the fit
-        glon, glat, vl = di['Glon'].values, di['Glat'].values, di['weighted_v_LSR'].values
+        glon, glat, vl = di['Glon'].values, di['Glat'].values, di[velocity_method + '_v_LSR'].values
         glon[glon>180] -= 360
         m = (np.abs(glat)<lat_lim)
         # Just storing the data in a Sightline object for convenience
